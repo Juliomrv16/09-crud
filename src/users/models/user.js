1c8de9089839e1d1ca9f45887437cdf2bdc5c633
Julio Rivas <juliomartinrv@hotmail.com>
2026-06-06 01:32:38 -0500

    User Model y problemas relacionados

---
+++ src/users/models/user.js
@@ -0,0 +1,19 @@
+//*Usaremos este archivo para adaptar los usuarios de la base
+//*de datos a unos usuarios propios
+
+
+export class User {
+    /**
+     * 
+     * @param {Like<User>} userDataLike 
+     */
+    constructor({ id, isActive, balance, avatar, firstName, lastName, gender }) {
+        this.id = id;
+        this.isActive = isActive;
+        this.balance = balance;
+        this.avatar = avatar;
+        this.first_name = firstName;
+        this.last_name =  lastName;
+        this.gender = gender;
+    }
+}
