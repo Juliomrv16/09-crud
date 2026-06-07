c4cce5177ebeb2e20f93685ddb63240661b9fbcf
Julio Rivas <juliomartinrv@hotmail.com>
2026-06-07 16:56:04 -0500

    Botones - Next, Prev y página actual comentarios

--- src/users/presentation/render-buttons/render-buttons.js
+++ src/users/presentation/render-buttons/render-buttons.js
@@ -2,16 +2,27 @@ import usersStore from "../../store/users-store";
 import "./render-buttons.css";
 
 /**
- *
+ *  La funcion crea los botenes, para cambiar de pagina, y un
+ *  span que indica en que pagina estamos
  * @param {HTMLDivElement} element
  */
 export const renderButtons = ( element ) => {
     
+    //*Se inicializa una constante "nexButton" donde 
+    //*se crea un boton y despues se indica lo que tendra
+    //*escrito
     const nexButton = document.createElement('button')
     nexButton.innerHTML = 'Next >';
+
+    //*Se inicializa una constante "nexButton" donde 
+    //*se crea un boton y despues se indica lo que tendra
+    //*escrito
     const prevButton = document.createElement('button')
     prevButton.innerHTML = '< Prev';
 
+    //*Se inicializa una constante "currentPageLabel" donde se
+    //*crea un "span" con un id "current-pag" y su contenido
+    //*sera la pagina donde se encuntre nuestros usuarios
     const currentPageLabel = document.createElement('span');
     currentPageLabel.id = 'current-pag';
     currentPageLabel.innerHTML = usersStore.getCurrentPage();
