96a70ff47b2ed431e2247029b42fd89bb3120959
Julio Rivas <juliomartinrv@hotmail.com>
2026-06-15 20:28:11 -0500

    Comportamiento esperado del modal

--- src/users/presentation/render-modal/render-modal.js
+++ src/users/presentation/render-modal/render-modal.js
@@ -1,19 +1,42 @@
 import modalHtml from "./render-modal.html?raw";
 import "./render-modal.css";
 
-let modal;
+let modal, form;
+
+//TODO: cargar usuario por id
+export const showModal = () => {
+    modal?.classList.remove('hide-modal');
+};
+
+export const hideModal = () => {
+    modal?.classList.add('hide-modal');
+    //Todo: Reset del formulario
+};
 
 /**
  *  Modal para crear un nuevo usuario
  * @param {HTMLDivElement} element
  */
 export const renderModal = ( element ) => {
-    
+
     if ( modal ) return;
 
     modal = document.createElement('div');
     modal.innerHTML = modalHtml;
     modal.className = 'modal-container hide-modal';
+    form = modal.querySelector('form');
+
+    modal.addEventListener('click',( event ) => {
+        if ( event.target.className === 'modal-container' ) {
+            hideModal();
+        }
+    });
+
+    form.addEventListener('submit',(event) => {
+        event.preventDefault();
+
+        console.log('Formulario enviado');
+    });
 
     element.append( modal );
 };
