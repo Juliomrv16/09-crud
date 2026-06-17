3efc0b8e61945ce76f731cdcd66c886dba0311b5
Julio Rivas <juliomartinrv@hotmail.com>
2026-06-16 23:02:12 -0500

    Cargar información del usuario al modal

---
+++ src/users/use-cases/get-user-by-id.js
@@ -0,0 +1,17 @@
+import { localhostUserToModel } from "../mappers/localhost-user.mapper";
+import { User } from "../models/user";
+
+
+/**
+ *  Nos da el usuario que hemos selecionado
+ * @param {String|Number} page
+ * @returns { promise<User> }
+ */
+export const getUserById = async( id ) => {
+    const url =`${ import.meta.env.VITE_BASE_URL }/users/${ id }`;
+    const res = await fetch(url);
+    const data = await res.json();
+    const user = localhostUserToModel( data );
+    console.log({user});
+    return user;
+};
